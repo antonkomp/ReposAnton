16a13c33ea2139b9ebd3fcf7cbabbc0230ce72dc
antonkomp <antonkomp@gmail.com>
2020-09-23 18:44:08 +0300

    oldrepos

---
+++ Os_System.py
@@ -0,0 +1,9 @@
+import os
+os.system('chcp 65001')
+print(os.system('help'))
+print(os.system('ipconfig'))
+print(os.system('dir'))
+os.system('cd Desktop:')
+print(os.system('dir'))
+#os.system(('rt.ico'))
+
