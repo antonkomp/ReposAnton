79d09bad56169c4b1543af96667740a0769fb2d6
antonkomp <68652543+antonkomp@users.noreply.github.com>
2021-04-24 11:04:52 +0200

    Delete Os_System.py

--- Os_System.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system('chcp 65001')
-print(os.system('help'))
-print(os.system('ipconfig'))
-print(os.system('dir'))
-os.system('cd Desktop:')
-print(os.system('dir'))
-#os.system(('rt.ico'))
-
